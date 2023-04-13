aae1dd7904e40206c1d5e6ee224cba4aa7dc803a
devashish gupta <89517503+Itsdevashishgupta@users.noreply.github.com>
2023-04-14 02:28:09 +0530

    update

--- Aaveg/src/app.js
+++ Aaveg/src/app.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const path=require("path");
 require("./db/conn");
+const User=require("./models/usermeg");
 const hbs=require("hbs");
 const app=express();
 const port=process.env.PORT || 3000;
@@ -9,9 +10,11 @@ const staticpath=path.join(__dirname,"../public");
 const templatepath=path.join(__dirname,"../templates/views");
 const partialpath=path.join(__dirname,"../templates/partials");
 //middlewares
+
 app.use('/css',express.static(path.join(__dirname,"../node_modules/bootstrap/dist/css")));
 app.use('/js',express.static(path.join(__dirname,"../node_modules/bootstrap/dist/js")));
 app.use('/jq',express.static(path.join(__dirname,"../node_modules/jquery/dist")));
+app.use(express.urlencoded({extended:false}))
 app.use(express.static(staticpath))
 app.set("view engine","hbs");
 app.set("views",templatepath);
@@ -21,8 +24,16 @@ hbs.registerPartials(partialpath)
 app.get("/",(req,res)=>{
        res.render("index");
 })
-app.get("/contact",(req,res)=>{ 
-       res.render("contact");
+
+app.post("/contact",async(req,res)=>{
+    try{
+res.send(req.body);
+const userData=new User(req.body);
+await userData.Save();
+res.status(201).render("index");
+    }catch(error){ 
+       res.status(500).send(error);
+    }
 })
 
 app.listen(port,()=>{
--- Aaveg/src/models/usermeg.js
+++ Aaveg/src/models/usermeg.js
@@ -4,6 +4,28 @@ const validator=require("validator");
 const userSchema=mongoose.Schema({
     name:{
         type:String,
-        require:true
+        require:true,
+        minLength:3
+    },
+    email:{
+       type:String,
+       required:true,
+       validate(value){
+        if(!validator.isEMail(value)){
+            throw new Error("Invalid email id")
+        }
+       }
+    },
+    phone:{
+        type:Number,
+        required:true,
+        min:10
+    },
+    message:{
+        type:String,
+        required:true,
+        minLength:3
     }
 })
+const User=mongoose.model("User",userSchema);
+module.exports=User;
